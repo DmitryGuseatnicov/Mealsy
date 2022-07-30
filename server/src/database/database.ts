e375270c24058646419e6baa0c67764d9522ae82
DmitryGuseatnicov <dimagusi111@gmail.com>
2022-07-31 00:52:21 +0300

    added eslint config

---
+++ server/src/database/database.ts
@@ -0,0 +1,8 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('test-db', 'user', 'pass', {
+  dialect: 'sqlite',
+  host: './dev.sqlite',
+});
+
+export default sequelize;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import sequelize from './database/database';
 
+sequelize.sync({ force: true }).then(() => console.log('db is ready'))
 const server = express();
 server.use(express.json())
 
-const PORT = 5000;
 
-server.listen(PORT, () => console.log(`Server working on port ${PORT}`))
+
+
+const PORT = 5002;
+
+
+server.listen(PORT, () => console.log(`Server working on port ${PORT}`));
