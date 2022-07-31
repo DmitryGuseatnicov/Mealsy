c5d30aea908b51ab987757f72a28f66603170f43
DmitryGuseatnicov <dimagusi111@gmail.com>
2022-08-01 02:49:08 +0300

    Refactor errorMiddlewere

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,15 +1,19 @@
+/* eslint-disable no-unused-vars */
+const errorAdapter = require('../adapters/errorAdapter');
 const ErrorCreator = require('../utils/ErrorCreator');
 
-// eslint-disable-next-line no-unused-vars
 const errorMiddleware = (error, req, res, next) => {
-  if (error instanceof ErrorCreator) {
-    return res.status(error.status).json({
-      message: error.message,
-      timestamp: Date.now(),
-      url: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
-    });
+  if (!(error instanceof ErrorCreator)) {
+    return res.status(500).json([{ message: 'Unknown error', error }]);
   }
-  return res.status(500).json({ message: 'Unknown error', error });
+
+  const isArray = Array.isArray(error.message);
+
+  return res.status(error.status).json({
+    message: isArray ? errorAdapter(error.message) : [error.message],
+    timestamp: Date.now(),
+    url: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
+  });
 };
 
 module.exports = errorMiddleware;
