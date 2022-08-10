354230b364680cb01244e001689e470160494b6a
DmitryGuseatnicov <dimagusi111@gmail.com>
2022-08-11 00:30:25 +0300

    Created func for chech and update arrays in json

--- server/utils/convertStrArrToArray.js
+++ server/utils/convertStrArrToArray.js
@@ -1,10 +1,5 @@
 const convertStrArrParams = (param) => {
-  /* if (typeof param === 'string') {
-    return JSON.parse(param);
-  }
-  return param; */
   return typeof param === 'string' ? JSON.parse(param) : param;
-
 };
 
 module.exports = convertStrArrParams;
