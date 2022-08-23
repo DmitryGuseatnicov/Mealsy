4ad3cbd5ddb1f8fec1dc98d07157bd93e0f3978e
DmitryGuseatnicov <dimagusi111@gmail.com>
2022-08-23 16:10:23 +0300

    Added functionlality of scroll category-feed with click on arrows

--- client/src/shared/ui/CategoryFeed/CategoryFeed.tsx
+++ client/src/shared/ui/CategoryFeed/CategoryFeed.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
+
+import Icon from '../Icon/Icon';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './CategoryFeed.scss';
@@ -27,6 +29,36 @@ const CategoryLink: FC<ICategoryLink> = (props) => {
   );
 };
 
+const PrevArrow = (props: any) => {
+  const { onClick } = props;
+
+  const classes = [
+    'category-feed__button-prev',
+    !onClick ? 'category-feed__button-prev_disable' : ''
+  ].join(' ');
+
+  return (
+    <button className={classes} type="button" onClick={onClick}>
+      <Icon name="arrow-forward" size="big" />
+    </button>
+  );
+};
+
+const NextArrow = (props: any) => {
+  const { onClick } = props;
+
+  const classes = [
+    'category-feed__button-next',
+    !onClick ? 'category-feed__button-next_disable' : ''
+  ].join(' ');
+
+  return (
+    <button className={classes} type="button" onClick={onClick}>
+      <Icon name="arrow-forward" size="big" />
+    </button>
+  );
+};
+
 const CategoryFeed: FC<ICategoryFeed> = (props) => {
   const { categories } = props;
 
@@ -35,7 +67,9 @@ const CategoryFeed: FC<ICategoryFeed> = (props) => {
       <Slider
         slidesToShow={7}
         swipeToSlide
-        infinite
+        infinite={false}
+        prevArrow={<PrevArrow />}
+        nextArrow={<NextArrow />}
         responsive={[
           {
             breakpoint: 1024,
